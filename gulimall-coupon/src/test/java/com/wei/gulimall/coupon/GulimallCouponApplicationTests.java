789bdff9bfbab77c0199ae4423075f2a64ee34b6
yaohongweiisme <2558939179@qq.com>
2022-09-04 00:42:18 +0800

    搭好骨架

---
+++ gulimall-coupon/src/test/java/com/wei/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wei.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ gulimall-order/src/test/java/com/wei/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wei.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
