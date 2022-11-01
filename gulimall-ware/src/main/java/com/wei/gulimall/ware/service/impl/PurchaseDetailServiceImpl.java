23e58fa4e2ec6123d0e52e6a68c0594ffc428152
yaohongweiisme <2558939179@qq.com>
2022-11-01 15:26:40 +0800

    完成库存服务的采购单合并功能

--- gulimall-ware/src/main/java/com/wei/gulimall/ware/service/impl/PurchaseDetailServiceImpl.java
+++ gulimall-ware/src/main/java/com/wei/gulimall/ware/service/impl/PurchaseDetailServiceImpl.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import java.util.Map;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.wei.common.utils.PageUtils;
